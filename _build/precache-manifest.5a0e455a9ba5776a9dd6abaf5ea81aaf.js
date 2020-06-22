a53af7828070d9c4cd1633b62b9c951f7dc536a3
Jirachai Chansivanon (Adecco Praram 4 Recruitment Li) <v-jircha@microsoft.com>
2020-06-22 13:07:45 +0000

    Create _build for publish

---
+++ _build/precache-manifest.5a0e455a9ba5776a9dd6abaf5ea81aaf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376ac6c8a6c69eb02975113522511331",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6545e51b82193546c99",
+    "url": "/static/css/main.737e73a0.chunk.css"
+  },
+  {
+    "revision": "112b7aedb8989289fdee",
+    "url": "/static/js/2.1548d243.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1548d243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6545e51b82193546c99",
+    "url": "/static/js/main.fa7bbcdf.chunk.js"
+  },
+  {
+    "revision": "dbf36e8acb27f11b698a",
+    "url": "/static/js/runtime-main.f8ae1765.js"
+  },
+  {
+    "revision": "5bcd457e76ec06791693776c843eca96",
+    "url": "/static/media/bob2020-bg.5bcd457e.jpg"
+  },
+  {
+    "revision": "cd2fd5e73ac34e7f60a68ac6e75e7537",
+    "url": "/static/media/logo.cd2fd5e7.svg"
+  }
+]);
