ebd5bb995166f8ce5570aebcdd3347f8ea9d177c
Jirachai Chansivanon (Adecco Praram 4 Recruitment Li) <v-jircha@microsoft.com>
2020-06-29 04:05:52 +0000

    Highlight everyone!

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello everybody. Welcome to Best of Build 2020</h1>
+        <h1>Hello <u>everybody</u>. Welcome to Best of Build 2020</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
